4694385c073b13dc9bf5d1b451999d48177346c7
Dave <dtaylor@zeavision.com>
2017-07-18 15:02:40 -0500

    Change cred

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function() {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-                                          appId: process.env.MICROSOFT_APP_ID,
-                                          appPassword: process.env.MICROSOFT_APP_PASSWORD
+                                          appId: null,
+                                          appPassword: null
                                           });
 
 // Listen for messages from users

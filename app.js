4e7f830dd6d1f7a3eaec9d3355dd060d8e4dc9c2
Dave <dtaylor@zeavision.com>
2017-07-19 09:54:26 -0500

    Add beginning of greetig dialog

--- app.js
+++ app.js
@@ -18,9 +18,18 @@ server.post('/api/messages', connector.listen())
 
 // Receive messages from the user and respond by echoing each message
 // back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-                                   session.sendTyping();
-                                   setTimeout(function () {
-                                              session.send("You said: %s", session.message.text);
+var bot = new builder.UniversalBot(connector, [
+                                   function (session, args, next) {
+                                               if (!session.userData.name){
+                                               session.beginDialog('profile');
+                                               } else {
+                                               next();
+                                               }
+                                               },
+                                               function (session,results){
+                                               session.sendTyping();
+                                               setTimeout(function () {
+                                              session.send('Hello %s', session.userData.name);
                                               }, 3000);
-});
+                                               }
+]);

78f13d5b5b343d3275ac7fcc2b611fa5bdd2b56c
Dave <dtaylor@zeavision.com>
2017-07-27 16:22:07 -0500

    add some stuff

--- app.js
+++ app.js
@@ -7,27 +7,22 @@ server.listen(process.env.port || process.env.PORT || 3978, function() {
     console.log('%s listening to %s', server.name, server.url);
 });
 
-// Create chat connector for communicating with the Bot Framework Service
+// Create chat bot
 var connector = new builder.ChatConnector({
     appId: 'c4ad429c-4046-4bf2-8d94-303aea58753c',
     appPassword: 'CLF3jnBrXFryCY3rZRARn27'
 });
 
-var bot = new builder.UniversalBot(connector);
 
-// Listen for messages on /api/messages
+// Listen for messages
 server.post('/api/messages', connector.listen());
 
-bot.dialog(function (message) {
-       if (identity.id === message.address.bot.id){
-       var name = message.user ? message.user.name : null;
-       var reply = new builder.Message()
-       .address(message.address)
-       .text("Hello %s. How are you today?", name || 'there');
-       bot.send(reply);
-       console.log(reply);
-       } else {
-            //delete their data
-       }
-       
-});
+//Setup Bot
+var bot = new builder.UniversalBot(connector, [
+        //Default Dialog
+                function(session){
+                     session.endDialog("Hello," + session.userData.name + ". Sakana is a bot that can help with a mulitude of tasks. Currently in Development.");
+                      builder.Prompts.choice(session, "Here are some functions being worked on:", "Salesforce|Hubspot|Analytics", builder.ListStyle.button);
+
+                }
+]);
